b0a152e914353fa8e63a8ecc932e4e6a488e3fa5
SafetyDuck <dimma5676@gmail.com>
2025-04-17 16:30:53 +0300

    Update main.go

--- main.go
+++ main.go
@@ -7,15 +7,16 @@ import (
 )
 
 func main() {
+	// Обрабатываются аргументы командной строки
 	port := flag.String("port", "8080", "port to listen on")
 	maxQueues := flag.Int("max-queues", 10, "maximum number of queues")
 	defaultSize := flag.Int("default-size", 100, "default queue size")
 	timeout := flag.Int("timeout", 5, "default timeout in seconds")
 	flag.Parse()
-
+	// Создается менеджер очередей и HTTP-сервер, запускается сервер
 	queueManager := NewQueueManager(*maxQueues, *defaultSize)
 	server := NewServer(queueManager, *port, time.Duration(*timeout)*time.Second)
-
+	
 	log.Printf("Starting server on port %s", *port)
 	if err := server.Start(); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
